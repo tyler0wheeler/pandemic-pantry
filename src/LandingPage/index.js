62ef55d7dc4c421518266b5333162419bd85e2e2
Tyler Wheeler <tyler0wheeler@gmail.com>
2020-12-09 13:39:05 -0500

    playing with css

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -15,7 +15,7 @@ export default function LandingPage(props){
             <p id="landing-paragraph">Many of us have found comfort in these troubling times in our food.  This site was created to showcase the recipes we have created with often little ingredients on hand.</p>
             </Fade>
             <Zoom>
-            <Image id="landing-page-image" src="https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></Image>
+            <Image className="landing-page-image" src="https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></Image>
             </Zoom>
             <Fade right>
             <p id="landing-paragraph">Use our Search to find recipes based on the ingredients you have from our friends at Spoonacular or register an account and create your own recipes for yourself or to share with our community!  We can't wait to see what you have been cooking up! </p>
--- src/SearchContainer/index.js
+++ src/SearchContainer/index.js
@@ -4,18 +4,6 @@ import { Form, Button} from 'semantic-ui-react'
 import SearchRecipeList from '../SearchRecipeList'
 import SingleSearchRecipe from '../SingleSearchRecipe'
 import SavedSearchedRecipes from '../SavedSearchedRecipes'
-// {
-//     "title":"Char-Grilled Beef Tenderloin with Three-Herb Chimichurri",
-//     "image":"https://spoonacular.com/recipeImages/char-grilled-beef-tenderloin-with-three-herb-chimichurri-156992.jpg",
-//     "servings":10,
-//     "readyInMinutes":45,
-//     "id":156992,
-//     "instructions":"PreparationFor spice rub: Combine all ingredients in small bowl. Do ahead: Can be made 2 days ahead. Store airtight at room temperature. For chimichurri sauce: Combine first 8 ingredients in blender; blend until almost smooth. Add 1/4 of parsley, 1/4 of cilantro, and 1/4 of mint; blend until incorporated. Add remaining herbs in 3 more additions, pureeing until almost smooth after each addition. Do ahead: Can be made 3 hours ahead. Cover; chill. For beef tenderloin: Let beef stand at room temperature 1 hour. Prepare barbecue (high heat). Pat beef dry with paper towels; brush with oil. Sprinkle all over with spice rub, using all of mixture (coating will be thick). Place beef on grill; sear 2 minutes on each side. Reduce heat to medium-high. Grill uncovered until instant-read thermometer inserted into thickest part of beef registers 130F for medium-rare, moving beef to cooler part of grill as needed to prevent burning, and turning occasionally, about 40 minutes. Transfer to platter; cover loosely with foil and let rest 15 minutes. Thinly slice beef crosswise. Serve with chimichurri sauce. *Available at specialty foods stores and from tienda.com.",
-//     "extendedIngredients":[{"originalString":"1/2-pound beef tenderloin"},
-//     {"originalString":"1/2-pound beef tenderloin"}
-//     ],
-
-// }
 
 export default class SearchContainer extends Component {
     constructor(props){
@@ -24,7 +12,6 @@ export default class SearchContainer extends Component {
             searchedRecipes: [],
             singleRecipe: null,
             savedRecipes: [],
-            savedIngredients: [],
             searchIngredients: '',
             conditionalView: 'search bar',
             idOfSearchedRecipeToShow: -1,
@@ -55,19 +42,17 @@ export default class SearchContainer extends Component {
                 'Content-Type': 'application/json'
 
             }
-        })
-        
-        console.log(response);
-        const responseJson = await response.json()
-        console.log(responseJson);
-        this.setState({
-            searchedRecipes: responseJson,
-            conditionalView: 'show search results'
-        }) 
-        }catch(err){
-            console.log("Error getting recipes in search", err);
-        }
-        }
+            })
+            const responseJson = await response.json()
+            console.log(responseJson);
+            this.setState({
+                searchedRecipes: responseJson,
+                conditionalView: 'show search results'
+            }) 
+            }catch(err){
+                console.log("Error getting recipes in search", err);
+            }
+            }
         getSingleSearchedRecipe = async (id) => {
             try{
             const response = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, {
@@ -78,7 +63,6 @@ export default class SearchContainer extends Component {
                     'Content-Type': 'application/json'
                 }
             })
-            console.log(response);
             const responseJson = await response.json()
             console.log(responseJson);
             this.setState({
@@ -91,87 +75,53 @@ export default class SearchContainer extends Component {
             }
         }
         getSavedRecipes = async () =>{
-        try{
-            const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/savedrecipes/"
-            const recipesResponse = await fetch(url,{
-                credentials: 'include'
-            })
-            const recipesJson= await recipesResponse.json()
-            this.setState({
-                savedRecipes: recipesJson.data.searchedrecipes,
-                savedIngredients: recipesJson.data.searchedingredients
-        })
-        } catch (err){
-            console.log("Error getting Saved Recipes data", err)
-        }
-       
-        console.log(this.state.savedRecipes);
-    }
-        saveRecipe = async (recipeToAdd) =>{
-        try{
-            const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/"
-            const savedRecipeResponse = await fetch(url,{
-                credentials: 'include',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(recipeToAdd)
-            })
-            const savedRecipeJson = await savedRecipeResponse.json()
-            console.log("This is the recipe being created", savedRecipeJson);
-            if(savedRecipeResponse.status === 200){
-                this.setState({
-                    savedRecipes: [...this.state.savedRecipes, savedRecipeJson.data],
-                    conditionalView: 'show saved recipes'
+            try{
+                const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/savedrecipes/"
+                const recipesResponse = await fetch(url,{
+                    credentials: 'include'
                 })
-            }
-        } catch(err){
-            console.log("Error saving recipe", err);
+                const recipesJson= await recipesResponse.json()
+                this.setState({
+                    savedRecipes: recipesJson.data.searchedrecipes,
+                    savedIngredients: recipesJson.data.searchedingredients
+                })
+                } catch (err){
+                    console.log("Error getting Saved Recipes data", err)
+                }
         }
-        this.getSavedRecipes()
-        console.log(this.state.savedRecipes);
-    }
-        // addIngredient = async (ingredientAdded, id) => {
-        //     try {
-        //         const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/searchedingredient/" + id
-        //         const addIngredientResponse = await fetch(url, {
-        //             credentials: 'include',
-        //             method: "POST",
-        //             body: JSON.stringify(ingredientAdded),
-        //             headers: {
-        //                 'Content-Type': 'application/json'
-        //             }
-        //         })
-        //         const addIngredientJson = await addIngredientResponse.json()
-        //         console.log("This is addIngredientJson", addIngredientJson);
-        //         if(addIngredientResponse.status === 200){
-        //             this.setState({
-        //                 savedIngredients: [...this.state.savedIngredients, addIngredientJson.data]
-        //             })
-        //         }
-        //     } catch(err){
-        //         console.log("Error adding ingredient", err);
-        //     }
-        //     this.getSavedRecipes()
-        // }
+        saveRecipe = async (recipeToAdd) =>{
+            try{
+                const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/"
+                const savedRecipeResponse = await fetch(url,{
+                    credentials: 'include',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(recipeToAdd)
+                })
+                const savedRecipeJson = await savedRecipeResponse.json()
+                console.log("This is the recipe being created", savedRecipeJson);
+                if(savedRecipeResponse.status === 200){
+                    this.setState({
+                        savedRecipes: [...this.state.savedRecipes, savedRecipeJson.data],
+                        conditionalView: 'show saved recipes'
+                    })
+                }
+            } catch(err){
+                console.log("Error saving recipe", err);
+            }
+            this.getSavedRecipes()
+        }
         deleteSavedRecipe = async (id, func) => {
             try {
                 const url = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/" + id
-                // const ingredientUrl = process.env.REACT_APP_API_URL + "/pandemic-pantry/searched-recipes/delete-all-ingredients/" + id
                 const deleteSavedRecipeResponse = await fetch(url, {
                     credentials: 'include',
                     method: "DELETE"
                 })
-                // const deleteSavedIngredientsWithRecipe = await fetch(ingredientUrl, {
-                //     credentials: 'include',
-                //     method: "DELETE"
-                // })
                 const deleteSavedRecipeJson = await deleteSavedRecipeResponse.json()
-                // const deleteSavedIngredientsJson = await deleteSavedIngredientsWithRecipe.json()
-                console.log("I want to delete this recipe: ", deleteSavedRecipeJson)
-                // console.log("I want to delete these ingredients: ", deleteSavedIngredientsJson)
-                if(deleteSavedRecipeJson.status) {
+                if(deleteSavedRecipeJson.status === 200 || deleteSavedRecipeJson.status === 201) {
                     
                     this.setState({
                         conditionalView: 'show saved recipes',
@@ -180,9 +130,7 @@ export default class SearchContainer extends Component {
                         })
                     this.setState({
                         savedRecipes: this.state.savedRecipes.filter(recipe => recipe.id !== id),
-                        // savedIngredients: this.state.savedIngredients.filter(ingredient => ingredient.recipe.id !== id)
-                    })
-                                
+                    })                              
                 }
             } catch(err) {
                 console.log("There was an error deleting the recipe", id)
@@ -190,20 +138,19 @@ export default class SearchContainer extends Component {
             this.getSavedRecipes()
         }
         showSingleSearchedRecipe = (id) => {
-        console.log("you are trying to show recipe with id: ", id)
-        this.getSingleSearchedRecipe(id)
-        this.setState({
-            idOfSearchedRecipeToShow: id,
-            conditionalView: 'single recipe view'
-        })
-        console.log(this.state.idOfRecipeToShow);
+            console.log("you are trying to show recipe with id: ", id)
+            this.getSingleSearchedRecipe(id)
+            this.setState({
+                idOfSearchedRecipeToShow: id,
+                conditionalView: 'single recipe view'
+            })
         }
         closeShowModal = () => {
-        this.setState({
-            idOfSearchedRecipeToShow: -1,
-            conditionalView: 'show search results'
-        })
-    }
+            this.setState({
+                idOfSearchedRecipeToShow: -1,
+                conditionalView: 'show search results'
+            })
+        }
         showSavedSearchRecipes = () =>{
           this.setState({
                 conditionalView: 'show saved recipes'
@@ -250,14 +197,9 @@ export default class SearchContainer extends Component {
                     &&
                 <Button id="show-saved-recipes" onClick={this.showSavedSearchRecipes}>Saved Searched Recipes</Button>
                 }
-            {/*<input name="word" type="search" value={this.state.searchIngredients} onChange={this.handleInputChange}/>
-            <input type="button" value="Search" onClick={this.search} />*/}
-            
-         
-           
             </Form>
             </Fade>
-             }
+            }
             {
                 this.state.conditionalView === "show search results"
                     &&
